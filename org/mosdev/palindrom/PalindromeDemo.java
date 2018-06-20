f8fb4e7fc6a99e9d7685e1471013bd34180aaef0
CHALLENGE-TONI <40395506+CHALLENGE-TONI@users.noreply.github.com>
2018-06-20 14:48:07 +0200

    Update PalindromeDemo.java

--- org/mosdev/palindrom/PalindromeDemo.java
+++ org/mosdev/palindrom/PalindromeDemo.java
@@ -3,11 +3,17 @@ package org.mosdev.palindrom;
 public class PalindromeDemo {
 
     public static void main(String[] args) {
+        // Create an instance of SentenceTool. Paramether is set to inputfile
         SentenceTool sentenceTool = new SentenceTool("D:\\Temp\\extradoc.txt");
 
+        // Loop, while sentenceTool has content in array
         while (sentenceTool.hasNextSentence()) {
+            // Save rawInput
             String rawInput = sentenceTool.nextSentence();
+            
+            // Is Palindrom?
             if (PalindromTest.isPalindrome(rawInput)) {
+                // Yes, output lineNumber and Raw Sentence without ?!., because by splitting it will removed.
                 System.out.println(sentenceTool.getCurrentLineNumber() + " " + rawInput);
             }
         }
