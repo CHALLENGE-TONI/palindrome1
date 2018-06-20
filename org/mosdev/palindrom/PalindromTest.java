f9ab819b0e25c086766e4bf2e9b61f27225fac30
CHALLENGE-TONI <40395506+CHALLENGE-TONI@users.noreply.github.com>
2018-06-20 14:46:07 +0200

    Update PalindromTest.java

--- org/mosdev/palindrom/PalindromTest.java
+++ org/mosdev/palindrom/PalindromTest.java
@@ -2,16 +2,24 @@ package org.mosdev.palindrom;
 
 public class PalindromTest {
 
+    // Returns true, when text is a palindrome
     public static boolean isPalindrome (String text) {
+        // Remove all spaces, .?! and other chars from String. Note: replaceAll with regex were better
         String preparedText = text.trim().toLowerCase().replace(",", "").replace(" ", "").replace("-", "").replace("â€“", "").replace("ã¤", "ä");
+        
+        // Set result as true
         boolean result = true;
 
-        for (int index = 0; index < preparedText.length(); index ++) {
-            char left = preparedText.charAt(index);
-            char right = preparedText.charAt(preparedText.length() - index - 1);
+        // Loop the whole perparedText and check left and right char at index position
+        for (int index = 0; index < preparedText.length(); index ++) {            
+            char left = preparedText.charAt(index); // start from left side of String
+            char right = preparedText.charAt(preparedText.length() - index - 1); // start from left side of String, subtract index and subtract -1, because when index is 0, and you try to get char at last position it is out of bounce!
 
+            // Is left char and right char equal?
             if (left != right) {
+                // No
                 result = false;
+                // Don't finish the loop
                 break;
             }
         }
